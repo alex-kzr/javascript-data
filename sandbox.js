ba4a65681efcf90a5ddc595d9aa1b84ff83310f8
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-16 17:35:49 +1000

    Add deleting localStorage data

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,6 @@
 // store data in local storage
-// localStorage.setItem('name', 'mario');
-// localStorage.setItem('age', '50');
+localStorage.setItem('name', 'mario');
+localStorage.setItem('age', '50');
 
 // get data from local storage
 let name = localStorage.getItem('name');
@@ -8,6 +8,8 @@ let age = localStorage.getItem('age');
 
 console.log(name, age);
 
-// updating data
-localStorage.setItem('name', 'luigi');
-localStorage.age = '40';
+// deleting data
+localStorage.removeItem('name');
+localStorage.clear();
+
+console.log(localStorage.getItem('name'));
